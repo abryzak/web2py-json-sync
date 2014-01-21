c6725c74be33c045cfb604f377ca484a1ac2de71
Adam Bryzak <adam@bryzak.net>
2014-01-21 17:13:21 +1000

    Add requirements

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@
 from distutils.core import setup
 
 setup(name='web2py-json-sync',
-      version='1.0',
+      version='1.1',
       description='Python Distribution Utilities',
       author='Adam Bryzak',
       author_email='adam@bryzak.net',
       py_modules=['json_sync'],
+      requires=['dateutil'],
      )
